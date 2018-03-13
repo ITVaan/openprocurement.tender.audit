657b0ceb7f360a16e25c8659cb10d93254f7a2aa
aprovotar <aprovotar@gmail.com>
2018-03-13 15:54:27 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1'
+
+requires = [
+    'setuptools',
+]
+
+test_requires = requires + [
+    'webtest',
+    'python-coveralls',
+    'openprocurement.tender.belowthreshold',
+]
+
+docs_requires = requires + [
+    'sphinxcontrib-httpdomain',
+]
+
+api_requires = requires + [
+    'openprocurement.api>=2.4',
+    'openprocurement.tender.core',
+]
+
+entry_points = {
+    'openprocurement.api.plugins': [
+        'tender.audit = openprocurement.tender.audit:includeme'
+    ],
+    # 'openprocurement.api.migrations': [
+    #     'audits = openprocurement.tender.audit.migration:migrate_data'
+    # ]
+}
+
+setup(name='openprocurement.tender.audit',
+      version=version,
+      description="",
+      long_description=open("README.md").read(),
+      classifiers=[
+        "Framework :: Pylons",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+        ],
+      keywords="web services",
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      license='Apache License 2.0',
+      url='https://github.com/ITVaan/openprocurement.tender.audit',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=test_requires,
+      extras_require={'api': api_requires, 'test': test_requires, 'docs': docs_requires},
+      entry_points=entry_points,
+      )
